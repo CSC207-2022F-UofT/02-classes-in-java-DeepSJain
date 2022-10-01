fd93841c97b483e80bddaf9b5689e1814401bb75
Your Name <you@example.com>
2022-10-01 14:10:26 -0400

    Fixed task 4

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -173,6 +173,8 @@ public class Silly implements Comparable<Silly>{
 
         // Hint: to compare strings, we need to use .equals()
         //       e.g. s1.equals(s2)
+
+        return this.name.equals(other.name);
     }
 
     /**
